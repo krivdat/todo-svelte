6d14e1f240a5f79cb27ba612e47063b680fe3759
Tomas Krivda <krivdat@gmail.com>
2022-02-23 14:20:45 +0100

    added password hashing and salting

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -39,9 +39,9 @@
         <li on:click={handleSignOut}>Sign out</li>
       {:else}
         <a href="/sign-in"><li>Sign in</li></a>
-        <!-- {#if $session.user.isAdmin}
-          <a href="/sign-up"><li>Sign up</li></a>
-        {/if} -->
+        <!-- {#if $session.user.isAdmin} -->
+        <a href="/sign-up"><li>Sign up</li></a>
+        <!-- {/if} -->
       {/if}
     </ul>
   </div>
